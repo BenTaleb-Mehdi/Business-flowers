f1586f4e2ce4bfc134e11e871cef9ecc68b997c8
Mehdy <mehdibentaleb548@gmail.com>
2026-06-17 02:24:30 +0100

    feat: initialize root layout with global providers, fonts, and SEO configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
           </main>
 
           {/* Global Footer */}
-          <Footer />
+        
         </LanguageProvider>
         </SmoothScroll>
             <FloatingContact />
